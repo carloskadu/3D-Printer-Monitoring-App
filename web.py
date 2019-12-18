ceba3c7b9288baba788c1a5f5fe527df689ef15d
Jokii <54998187+Joookii@users.noreply.github.com>
2019-12-18 18:38:34 -0300

    leitura
    Código adaptado para leitura de dados do servidor

--- web.py
+++ web.py
@@ -4,13 +4,11 @@ import json
 # Consumindo WebAPI
 
 while True:
-    cep = input('Digite o CEP para ter informações sobre o local: ')
-    
-    #URL
-    url_cep = "http://viacep.com.br/ws/{}/json/".format(cep)
+    #URL --> INSIRA O IP AQUI
+    url = "IP:PORTA"
 
     #Buscando o URL do WebAPI
-    req = requests.get(url_cep)
+    req = requests.get(url)
 
     #Tentativa de acesso
     try:
@@ -20,11 +18,4 @@ while True:
         break
 
     except json.decoder.JSONDecodeError:
-        print("CEP inválido, digite novamente.")
         continue
-
-#Exibição dos dados
-print('CEP: {}'.format(adress_data['cep']))
-print('CIDADE: {}'.format(adress_data['localidade']))
-print('BAIRRO: {}'.format(adress_data['bairro']))
-print('ESTADO: {}'.format(adress_data['uf']))
