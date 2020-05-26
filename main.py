ac92771b2854ffd84024e71fa255ab1016eb1c53
carloskadu <54999482+carloskadu@users.noreply.github.com>
2020-05-26 18:37:21 -0300

    More realibles names for easy understand

--- main.py
+++ main.py
@@ -31,19 +31,19 @@ def changeDataInServer(key, new_value):
 
 def main():
     changeDataInServer('code', 'on')
-    C = datetime.datetime.now() # Your local time
-    print(C)
+    time = datetime.datetime.now() # Your local time
+    print(time)
     # Video Capture 
     capture = cv2.VideoCapture(0)
 
     # History, Threshold, DetectShadows 
     fgbg = cv2.createBackgroundSubtractorMOG2(300, 400, True)
 
-    s = "The impression has started!" # Says if it is moving or not
+    status = "The impression has started!" # Says if it is moving or not
     i=0
     f=0
     z=0
-    T = "Not over" # Say the progress of the impression
+    end = "Not over" # Say the progress of the impression
 
     while(1):
         # Return Value and the current frame
@@ -65,31 +65,31 @@ def main():
 
         # Determine how many pixels do you want to detect to be considered "movement"
         if count <= 500 : # Condition to see if the movement has stopped and start the countdown
-            if s != "Moving":
+            if status != "Moving":
                 t=i
                 t1=datetime.datetime.now()
                 i=int(t1.second)
-                if t!=i and T != "End" :
+                if t!=i and end != "End" :
                     z+=1
-                    if z==20 and T == "Not over":
-                        N = datetime.datetime.now()
-                        M = N - C
+                    if z==20 and end == "Not over":
+                        finalTime = datetime.datetime.now()
+                        deltaTime = finalTime - time
                         print("Printing is done!")
-                        print('Time:{}'.format(M))
+                        print('Time:{}'.format(deltaTime))
                         changeDataInServer('switch', 'off')
                         f=0
                         i=0 
                         z=0
-                        T = "End"
+                        end = "End"
                 
         else:
             z=0
-            T = "Not over"
-            if s != "Stopped":
+            end = "Not over"
+            if status != "Stopped":
                 changeDataInServer('switch', 'on')
-                print(s)
-                s = "Stopped"
-                T = "Not over"
+                print(status)
+                status = "Stopped"
+                end = "Not over"
 
         cv2.imshow('Frame', resizedFrame)
         cv2.imshow('Mask', fgmask)
